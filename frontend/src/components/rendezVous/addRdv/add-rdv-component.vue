5d5736d5f3c21de5b5680940d7e8e409233808ee
Thomas Candille <thomascandille0508@gmail.com>
2026-01-29 13:22:55 +0100

    feat: Add addRdvComponent to Rendez-vous component for new appointment functionality

---
+++ frontend/src/components/rendezVous/addRdv/add-rdv-component.vue
@@ -0,0 +1,55 @@
+<script>
+export default {
+    name: 'addRdvComponent',
+    methods: {
+        handleClick() {
+            alert('Ajouter un nouveau rendez-vous');
+        }
+    }
+}
+</script>
+
+<template>
+
+    <div class="add-rdv-container">
+        <div class="add-rdv-text">
+            <h2>Rendez vous</h2>
+            <p>2 à venir </p>
+        </div>
+        <button class="add-rdv-button" @click="handleClick"> + </button>
+    </div>
+
+</template>
+
+<style scoped>
+
+.add-rdv-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 16px;
+}
+
+.add-rdv-text h2 {
+    margin: 0;
+    font-size: 24px;
+    color: #0f2722;
+}
+.add-rdv-text p {
+    margin: 4px 0 0 0;
+    font-size: 14px;
+    color: #6b7280;
+}
+
+.add-rdv-button {
+    width: 40px;
+    height: 40px;
+    border-radius: 20px;
+    background-color: #3a8d7a;
+    color: white;
+    font-size: 24px;
+    border: none;
+    cursor: pointer;
+}
+
+</style>
